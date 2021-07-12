22ac44b549864e0d1c6d1dc9cf274494dff9508d
basicjohn <edmondsonj@gmail.com>
2021-07-12 15:12:54 -0700

    create reducer & 2 actions

---
+++ src/reducers/beer-list-reducer.js
@@ -0,0 +1,25 @@
+export default (state = {}, action) => {
+  const {id, department, name, pricePerUnit, unitsPerKeg, numberOfKegs, costPerKeg, abv, ibu, description} = action;
+  switch (action.type) {
+    case 'ADD_BEER':
+      return Object.assign({}, state, {
+        [id]: {
+          id: id,
+          department: department,
+          name: name,
+          pricePerUnit: pricePerUnit,
+          unitsPerKeg: unitsPerKeg,
+          numberOfKegs: numberOfKegs,
+          costPerKeg: costPerKeg,
+          abv: abv,
+          ibu: ibu,
+          description: description
+        }
+      });
+      case 'DELETE_BEER':
+        const newState = { ...state };
+        delete newState[id];
+      default:
+        return state;
+  }
+};

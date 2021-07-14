1c35149349d13a331016d7bd2a89f2c4c5c350d3
basicjohn <edmondsonj@gmail.com>
2021-07-13 18:36:28 -0700

    reworking the second test to work with department names

--- src/__tests__/reducers/beer-list-reducer.test.js
+++ src/__tests__/reducers/beer-list-reducer.test.js
@@ -6,47 +6,82 @@ describe('beerListReducer', () => {
 
   const currentState = {
     1: {
-      "id": "df3dab63-1f67-43b6-ab5c-5065cd864814",
-      "name": "IRA",
-      "pricePerUnit": 5,
-      "unitsLeftInKeg": 124,
-      "unitsPerKeg": 124,
-      "numberOfKegs": 3,
-      "costPerKeg": 248,
-      "abv": 6.5,
-      "ibu": 65,
-      "description": "Our India Red Ale uses plenty of Northwest-grown, Brewer’s Gold and Simcoe hops. With a solid foundation of Pilsner and crystal malts and our inimitable house yeast, these ingredients intertwine in delicious complexity and a shining ruby hue."
+      id: "df3dab63-1f67-43b6-ab5c-5065cd864814",
+      department: "seasonal",
+      name: "IPA",
+      pricePerUnit: 5,
+      unitsLeftInKeg: 124,
+      unitsPerKeg: 124,
+      numberOfKegs: 3,
+      costPerKeg: 248,
+      abv: 6.5,
+      ibu: 65,
+      description: "Our India Red Ale uses plenty of Northwest-grown, Brewer’s Gold and Simcoe hops. With a solid foundation of Pilsner and crystal malts and our inimitable house yeast, these ingredients intertwine in delicious complexity and a shining ruby hue."
     },
     2: {
-      "id": "53e5bc74-26e9-4228-a4b6-6b0929695772",
-      "name": "Kolsch",
-      "pricePerUnit": 5,
-      "unitsLeftInKeg": 124,
-      "unitsPerKeg": 124,
-      "numberOfKegs": 3,
-      "costPerKeg": 248,
-      "abv": 5.2,
-      "ibu": 40,
-      "description": "This unfiltered Ale features a unique yeast strain, providing restrained fruited aromas to complement the rounded malt quality; with a touch more hop character than its counterpart in Cologne. Crisp, dry, and highly refreshing."
+      id: "53e5bc74-26e9-4228-a4b6-6b0929695772",
+      department: "seasonal",
+      name: "Kolsch",
+      pricePerUnit: 5,
+      unitsLeftInKeg: 124,
+      unitsPerKeg: 124,
+      numberOfKegs: 3,
+      costPerKeg: 248,
+      abv: 5.2,
+      ibu: 40,
+      description: "This unfiltered Ale features a unique yeast strain, providing restrained fruited aromas to complement the rounded malt quality; with a touch more hop character than its counterpart in Cologne. Crisp, dry, and highly refreshing."
     }
   };
 
 const beerData = {
-    "id": "df3dab63-1f67-43b6-ab5c-5065cd864814",
-    "name": "IRA",
-    "pricePerUnit": 5,
-    "unitsLeftInKeg": 124,
-    "unitsPerKeg": 124,
-    "numberOfKegs": 3,
-    "costPerKeg": 248,
-    "abv": 6.5,
-    "ibu": 65,
-    "description": "Our India Red Ale uses plenty of Northwest-grown, Brewer’s Gold and Simcoe hops. With a solid foundation of Pilsner and crystal malts and our inimitable house yeast, these ingredients intertwine in delicious complexity and a shining ruby hue."
+    id: "df3dab63-1f67-43b6-ab5c-5065cd864814",
+    department: "seasonal",
+    name: "IRA",
+    pricePerUnit: 5,
+    unitsLeftInKeg: 124,
+    unitsPerKeg: 124,
+    numberOfKegs: 3,
+    costPerKeg: 248,
+    abv: 6.5,
+    ibu: 65,
+    description: "Our India Red Ale uses plenty of Northwest-grown, Brewer’s Gold and Simcoe hops. With a solid foundation of Pilsner and crystal malts and our inimitable house yeast, these ingredients intertwine in delicious complexity and a shining ruby hue."
 };
 
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(beerListReducer({}, {type: null})).toEqual({});
   });
-  // test('Should successfully add new beer data to beersTicketList')
+  test('Should successfully add new beer data to beersTicketList', () => {
+    const { id, department, name, pricePerUnit, unitsLeftInKeg, unitsPerKeg, numberOfKegs, costPerKeg, abv, ibu, description} = beerData;
+    action = {
+      type: 'ADD_TICKET',
+      id: id,
+      department: department,
+      name: name,
+      pricePerUnit: pricePerUnit,
+      unitsLeftInKeg: unitsLeftInKeg,
+      unitsPerKeg: unitsPerKeg,
+      numberOfKegs: numberOfKegs,
+      costPerKeg: costPerKeg,
+      abv: abv,
+      ibu: ibu,
+      description: description
+    };
+
+    expect(beerListReducer({}, action)).toEqual({
+      [id] : {
+        id: id,
+        department: department,
+        name: name,
+        pricePerUnit: pricePerUnit,
+        unitsLeftInKeg: unitsLeftInKeg,
+        unitsPerKeg: unitsPerKeg,
+        numberOfKegs: numberOfKegs,
+        costPerKeg: costPerKeg,
+        abv: abv,
+        ibu: ibu,
+        description: description
+      }
+    });
+  });
 
 });
